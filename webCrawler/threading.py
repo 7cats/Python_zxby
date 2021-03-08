a02b6e9d40fdc93e7ea50106214759d4f4fb60fb
boyao <>
2021-03-08 21:09:54 +0100

    add print to testBranch

--- webCrawler/threading.py
+++ webCrawler/threading.py
@@ -8,13 +8,15 @@ Created on Mon Nov  9 23:29:08 2020
 import threading
 
 class messenger(threading.Thread):
-    
+
     def run(self):
         for _ in range(100):
             print(threading.currentThread().getName())
-            
+
 thread1 = messenger(name = 'send')
 thread2 = messenger(name = 'receive')
 
 thread1.start()
 thread2.start()
+
+print("to test branch");
